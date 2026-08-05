a2c83bec9d64c8694e9d455335141cef36639ce6
Hammad Talat <hammad.baig874@gmail.com>
2026-08-05 11:23:06 +0500

    initialized frontend

---
+++ frontend/src/routes/RoutePaths.js
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    HOME: "/",
+    APPLICATION_STATUS: "/application-status",
+
+    ADMIN_HOME: "/admin",
+    ADMIN_PENDING_USERS: "/admin/pending-users",
+    ADMIN_ACCOUNTS: "/admin/accounts",
+
+    ACCOUNT_HOME: "/account",
+};
