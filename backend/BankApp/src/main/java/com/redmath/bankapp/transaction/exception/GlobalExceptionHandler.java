c579bbb3a50cbee7b6931bb39365667154911ef5
Talha Ahmed <123talhaahmed@gmail.com>
2026-08-05 16:16:45 +0500

    created new endpoint /api/v1/account/balance get to get  the current balance of a user. added relevant tests for service and controller methods- all passing

--- backend/BankApp/src/main/java/com/redmath/bankapp/transaction/exception/GlobalExceptionHandler.java
+++ backend/BankApp/src/main/java/com/redmath/bankapp/transaction/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.redmath.bankapp.transaction.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.security.auth.login.AccountNotFoundException;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -17,4 +19,12 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(Map.of("error", ex.getMessage()));
     }
+
+    @ExceptionHandler(BalanceNotFoundException.class)
+    public ResponseEntity<Map<String, String>>  handleBalanceNotFound(BalanceNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error", ex.getMessage()));
+    }
+
 }
