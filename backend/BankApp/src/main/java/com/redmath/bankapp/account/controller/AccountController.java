804d1eb9561076123ae182e0f2fc8e7d163a3816
Talha Ahmed <123talhaahmed@gmail.com>
2026-08-06 17:09:35 +0500

    added endpoint to get account info: accountNumber and status

--- backend/BankApp/src/main/java/com/redmath/bankapp/account/controller/AccountController.java
+++ backend/BankApp/src/main/java/com/redmath/bankapp/account/controller/AccountController.java
@@ -1,9 +1,9 @@
 package com.redmath.bankapp.account.controller;
 
 
+import com.redmath.bankapp.account.dto.AccountResponse;
 import com.redmath.bankapp.account.dto.BalanceResponse;
-import com.redmath.bankapp.account.service.AccountBalanceService;
-import com.redmath.bankapp.tempconfig.security.UserPrincipal;
+import com.redmath.bankapp.account.service.AccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -14,17 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.security.auth.login.AccountNotFoundException;
 
 @RestController
-@RequestMapping("/api/v1/account/")
-public class AccountBalanceController {
+@RequestMapping("/api/v1/account")
+public class AccountController {
 
-    private final AccountBalanceService accountBalanceService;
+    private final AccountService accountService;
 
-    public AccountBalanceController(AccountBalanceService accountBalanceService) {
-        this.accountBalanceService = accountBalanceService;
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
     }
 
     @GetMapping("/balance")
     public ResponseEntity<BalanceResponse> getBalance(@AuthenticationPrincipal Jwt jwt) throws AccountNotFoundException {
-        return ResponseEntity.ok(accountBalanceService.getBalance(jwt));
+        return ResponseEntity.ok(accountService.getBalance(jwt));
     }
+
+    @GetMapping()
+    public  ResponseEntity<AccountResponse> getAccount(@AuthenticationPrincipal Jwt jwt) throws AccountNotFoundException
+    {
+        return ResponseEntity.ok(accountService.getAccount(jwt));
+    }
+
 }
