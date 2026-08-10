701f8dd94ad2d3041e88564be0d2e7c921889085
Talha Ahmed <123talhaahmed@gmail.com>
2026-08-10 10:49:08 +0500

    removed unused imports

--- backend/BankApp/src/main/java/com/redmath/bankapp/transaction/exception/GlobalExceptionHandler.java
+++ backend/BankApp/src/main/java/com/redmath/bankapp/transaction/exception/GlobalExceptionHandler.java
@@ -1,16 +1,13 @@
 package com.redmath.bankapp.transaction.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.security.auth.login.AccountNotFoundException;
-import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestControllerAdvice
