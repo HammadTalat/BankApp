3febba183af8cc9a00060b7585f3fde6fb2612e5
Talha Ahmed <123talhaahmed@gmail.com>
2026-08-18 15:13:47 +0500

    deleted application-test.yaml, moved content to application.yaml inside the test package

--- backend/BankApp/src/test/java/com/redmath/bankapp/account/repository/AccountBalanceRepositoryTest.java
+++ backend/BankApp/src/test/java/com/redmath/bankapp/account/repository/AccountBalanceRepositoryTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @Transactional
 class AccountBalanceRepositoryTest {
 
